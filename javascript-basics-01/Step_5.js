c6f4bb385414f98f773277a986295222bc148375
kawthar <kawtharzeayter.6@gmail.com>
2021-05-30 03:56:45 +0300

    Done Step_5

--- javascript-basics-01/Step_5.js
+++
@@ -1,6 +0,0 @@
-function data(){
-      var first_number=document.getElementById("first_number").value
-      var second_number=document.getElementById("second_number").value
-      var total=first_number * second_number;
-      alert(first_number +" X " +second_number + " = " + total)
-} 
