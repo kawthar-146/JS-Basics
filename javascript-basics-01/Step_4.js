8dd44a2c8878a48eabfd1eafa1051b2c8bddc21e
kawthar <kawtharzeayter.6@gmail.com>
2021-05-30 03:54:53 +0300

    Done Step_4

--- javascript-basics-01/Step_4.js
+++
@@ -1,7 +0,0 @@
-function info(){
-    var name=document.getElementById("name").value
-    var surname=document.getElementById("surname").value
-    var city=document.getElementById("city").value
-    alert("name: "+ name +"\n" +"surname: "+ surname + "\n" +"city: " +city)
-
-     }
