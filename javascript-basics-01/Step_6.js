1ecc0f0f325833c7cefa8626482831cc9c847d5a
kawthar <kawtharzeayter.6@gmail.com>
2021-05-30 04:01:20 +0300

    Done Step_6

---
+++ javascript-basics-01/Step_6.js
@@ -0,0 +1,7 @@
+function data(){
+    var first_number=document.getElementById("first_number").value
+    var second_number=document.getElementById("second_number").value
+    var total=first_number / second_number;
+    alert(first_number +" / " +second_number + " = " + total) 
+
+     }
