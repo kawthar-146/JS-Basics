0dc98d6b4605a00f2a8b171cbe2c20ce6bc01478
kawthar <kawtharzeayter.6@gmail.com>
2021-05-30 05:26:42 +0300

    Done Step 2

---
+++ javascript-basics-02/step 2/Step_2.js
@@ -0,0 +1,9 @@
+const input = document.querySelector("#name");
+
+input.addEventListener("blur", () => {
+    alert("Thank you for practicing");
+});
+function myFunction() {
+    var x = document.getElementById("fname");
+    x.value = alert("Thank you for practicing");
+  }
