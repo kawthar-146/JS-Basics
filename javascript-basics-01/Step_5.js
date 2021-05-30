79c624beaf084b732d682755745ff9c069d44ea1
kawthar <kawtharzeayter.6@gmail.com>
2021-05-30 03:42:04 +0300

    Done Step_5

---
+++ javascript-basics-01/Step_5.js
@@ -0,0 +1,6 @@
+function data(){
+      var first_number=document.getElementById("first_number").value
+      var second_number=document.getElementById("second_number").value
+      var total=first_number * second_number;
+      alert(first_number +" X " +second_number + " = " + total)
+} 
